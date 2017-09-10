e1a362d0825d3444d2e512db28fa0b550bf0a130
Gleb Skibitsky <gl.skib@gmail.com>
2017-09-10 16:33:34 +0300

    Project upload

---
+++ Assets/lizzard/Scripts/Properties/GameObjectProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace lizzard
+{
+    public class GameObjectProperties : MonoBehaviour
+    {
+        public bool IsActive
+        {
+            get { return gameObject.activeSelf; }
+            set { gameObject.SetActive(value); }
+        }
+
+        public string tag
+        {
+            get { return gameObject.tag; }
+            set { gameObject.tag = value; }
+        }
+    }
+
+}
